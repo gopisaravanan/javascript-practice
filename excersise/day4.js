3968e3ed86f6f8684045bb9923671d1aa481a9f1
Gopi saravanan R <gopisaravanan1398@gmail.com>
2026-02-02 22:59:37 +0530

    remove duplicates

--- excersise/day4.js
+++ excersise/day4.js
@@ -44,3 +44,22 @@ const arr = [2,4,6,[3,7,[9,8]]]
 
 console.log(arr.flat())
 console.log(arr.flat(Infinity))
+
+// Remove duplicates
+
+
+// const arr = [1, 2, 2, 3, 4,4];
+const removeDuplicates = (arr) => {
+    const result = [];
+    
+    for(let i = 0; i < arr.length; i++){
+        if(!result.includes(arr[i])){
+            result.push(arr[i])
+        }
+    }
+    
+    return result;
+    
+}
+
+console.log(removeDuplicates(arr))
