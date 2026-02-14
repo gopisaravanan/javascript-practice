f66d1099e0a4d2e44e25806b8b84c5c7c3718616
Gopi saravanan R <gopisaravanan1398@gmail.com>
2026-02-14 20:40:15 +0530

    char freq

--- excersise/day8.js
+++ excersise/day8.js
@@ -33,3 +33,18 @@ const counter = outer()
 console.log(counter())
 console.log(counter())
 
+// Char Freq
+const str = 'javascript';
+
+const freq = (str)=> {
+    const map = {};
+    
+    for (let ch of str){
+        map[ch] = (map[ch] || 0) + 1
+    }
+    
+    return map;
+}
+
+console.log(freq(str))
+
