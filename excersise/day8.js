ddaf0a49ba5c5ade9e08131c39467e2d6be232c9
Gopi saravanan R <gopisaravanan1398@gmail.com>
2026-02-16 14:25:01 +0530

    map - polyfill

--- excersise/day8.js
+++ excersise/day8.js
@@ -76,3 +76,16 @@ const findMaxVal = (arr) => {
 }
 
 console.log(findMaxVal(arr))
+
+// const arr = [1,2,3,4]
+
+
+Array.prototype.myMap = function (cb) {
+    let result = []
+    for(let i =0; i < this.length; i++){
+        result.push(cb(this[i], i, this))
+    }
+    return result;
+}
+
+console.log(arr.myMap((val) => val * 2))
