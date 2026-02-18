57f1448f114bf205bce4687826b0178b577b74f4
Gopi Saravanan R <gopisaravanan1398@gmail.com>
2026-02-18 23:58:19 +0530

    max occur freq

--- excersise/day9.js
+++ excersise/day9.js
@@ -25,3 +25,31 @@ const checkSalaryUpdate = arr1.map((val) => {
 
 
 console.log(checkSalaryUpdate)
+
+
+// max freq
+const str = 'aaabbbcccdddaaa';
+
+const maxFreqChar = (str) => {
+    const map = {};
+
+    // build frequency map
+    for (let ch of str) {
+        map[ch] = (map[ch] || 0) + 1;
+    }
+
+    // find max occurring character
+    let maxChar = '';
+    let maxCount = 0;
+
+    for (let key in map) {
+        if (map[key] > maxCount) {
+            maxCount = map[key];
+            maxChar = key;
+        }
+    }
+
+    return { char: maxChar, count: maxCount };
+};
+
+console.log(maxFreqChar(str));
