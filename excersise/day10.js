cb955ebe447696320fffcc9306f8b6744c9fcdc1
Gopi Saravanan R <gopisaravanan1398@gmail.com>
2026-02-19 20:17:47 +0530

    add binary search implementation

---
+++ excersise/day10.js
@@ -0,0 +1,26 @@
+// Binary search
+
+const arr = [ 2, 3, 7, 11, 15, 25]
+
+const target = 7;
+
+const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length-1;
+    
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2)
+        if(arr[mid] === target){
+            return mid;
+        }else if(arr[mid] < target){
+            left = mid + 1
+        }else{
+            right = mid -1
+        }
+    }
+    
+    return -1
+    
+}
+
+console.log(binarySearch(arr, target))
