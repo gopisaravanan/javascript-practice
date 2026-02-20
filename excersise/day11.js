730fd1c11f6059216c383d9a56fc924142d930f5
Gopi Saravanan R <gopisaravanan1398@gmail.com>
2026-02-20 15:52:00 +0530

    bubble sort

--- excersise/day11.js
+++ excersise/day11.js
@@ -53,3 +53,23 @@ const removeDuplicate = (str) => {
 
 
 console.log(removeDuplicate(str))
+
+// bubble sort
+
+// const arr = [5,3,4,1,2,6];
+
+const bubbleSort = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length; j++){
+            if(arr[i] < arr[j]){
+                let temp = arr[i];
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+        return arr;
+    
+}
+
+console.log(bubbleSort(arr))
