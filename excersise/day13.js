78faaf525346a5d4a02383e6d6e0ef0e1602011f
Gopi Saravanan R <gopisaravanan1398@gmail.com>
2026-02-23 01:14:00 +0530

    binary search

--- excersise/day13.js
+++ excersise/day13.js
@@ -53,3 +53,26 @@ const twoSum = (arr, target) => {
 
 console.log(twoSum(arr, target))
 
+// binary search
+
+// const arr = [2,3,4,5,8,9]
+
+// const target = 8;
+
+const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length -1;
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2)
+        if(arr[mid] === target){
+            return mid;
+        }else if(arr[mid] < target){
+            left = mid+1
+        }else{
+            right = mid -1
+        }
+    }
+    
+}
+
+console.log(binarySearch(arr, target))
