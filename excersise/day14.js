a10091e982816366f84f1357d470e9170c871c83
Gopi Saravanan R <gopisaravanan1398@gmail.com>
2026-02-25 23:38:13 +0530

    bubble sort

---
+++ excersise/day14.js
@@ -0,0 +1,17 @@
+const arr = [4,3,5,6,1,9];
+
+const bubbleSort = (arr) => {
+    for(let i =0; i < arr.length; i++){
+        for(let j =0; j < arr.length; j++){
+            if(arr[i] < arr[j]){
+                let temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp
+            }
+        }
+    }
+    return arr;
+    
+}
+
+console.log(bubbleSort(arr))
