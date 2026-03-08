dd1d8da7533828027bd0a5e0acec0a9d52c3e340
Gopi Saravanan R <gopisaravanan1398@gmail.com>
2026-03-08 22:37:26 +0530

    filter polyfill

--- excersise/day15.js
+++ excersise/day15.js
@@ -12,3 +12,26 @@ Array.prototype.myMap = function (cb) {
 }
 
 console.log(arr.myMap((val) => val * 2))
+
+
+// filter polyfillß
+
+const arr1 = [2,3,4,5,6];
+
+Array.prototype.myFilter = function (cb) {
+    let result = [];
+    for(let i = 0; i < this.length; i++){
+        if(cb(this[i],i,this)){
+            result.push(this[i])
+        }
+    }
+    return result;
+    
+}
+
+const findEven = (arr1) => {
+    return arr1.myFilter((val) => val % 2 === 0)
+    
+}
+
+console.log(findEven(arr1))
